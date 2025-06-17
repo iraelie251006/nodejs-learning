76749e455fa266455e516baffa03f32830ceeada
Elie <niyubwayoiraelie5777@gmail.com>
2025-06-17 12:32:32 +0200

    learnt REPL, node and browser objects

---
+++ REPL/REPL.js
@@ -0,0 +1,4 @@
+// REPL stands for Read Eval Print Loop
+// what this it does is it reads the input from the user, evaluates it, and prints the result
+// it keeps doing this until the user stops it
+// for example if the user types 1 + 1, it will print 2
---
+++ index.js
@@ -0,0 +1,19 @@
+// The global object is the top-level object in JavaScript. Every environment (like the browser or Node.js) has its own global object.
+// 🌍 Different Environments, Different Names:
+// Environment	Global Object Name
+// Browser window or self or globalThis
+// Node.js	global or globalThis
+
+
+// Key Properties of the Global Object:
+// Built-in functions: setTimeout(), parseInt(), isNaN()
+
+// Constructors: Object, Array, Function, Date
+
+// Global variables you declare with var (but not with let or const)
+
+// console.log(process)
+
+const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+const randomNumber = generateRandomNumber();
+console.log(`Celcius: ${randomNumber} -> Fahrenheit: ${celciusToFahrenheit(randomNumber)}`);
